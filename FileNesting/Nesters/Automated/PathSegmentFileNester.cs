f7afecb1d601f39f361a23be0cb12e21dc633d9b
Floatas <floatas@outlook.com>
2015-11-07 21:09:07 +0200

    Added support for .tsx files

--- FileNesting/Nesters/Automated/PathSegmentFileNester.cs
+++ FileNesting/Nesters/Automated/PathSegmentFileNester.cs
@@ -1,6 +1,6 @@
-﻿using System.IO;
+﻿using EnvDTE;
+using System.IO;
 using System.Linq;
-using EnvDTE;
 
 namespace MadsKristensen.FileNesting
 {
@@ -35,7 +35,7 @@ namespace MadsKristensen.FileNesting
         private static bool IsSupported(string fileName)
         {
             string extension = Path.GetExtension(fileName).ToLowerInvariant();
-            string[] allowed = new[] { ".js", ".css", ".html", ".htm", ".less", ".scss", ".coffee", ".iced", ".config", ".cs", "vb" };
+            string[] allowed = new[] { ".js", ".css", ".html", ".htm", ".less", ".scss", ".coffee", ".iced", ".config", ".cs", "vb", ".tsx" };
 
             return allowed.Contains(extension);
         }
